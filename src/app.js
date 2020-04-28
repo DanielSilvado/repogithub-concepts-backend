291bb78d08166a387c944fffa5d6e6b1b29ec5e9
daniel.grisolia_storm <daniel.grisolia_storm@prestadortvg.com.br>
2020-04-27 22:19:04 -0300

    middleware funtion validate id

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,17 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateId(request, response, next){
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid project ID'});
+  }
+
+  return next();
+
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories)
@@ -29,7 +40,7 @@ app.post("/repositories", (request, response) => {
 
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", validateId ,(request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body
 
@@ -53,7 +64,7 @@ app.put("/repositories/:id", (request, response) => {
   return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", validateId ,(request, response) => {
   const { id } = request.params;
 
     const repositoryIndex = repositories.findIndex(repository => repository.id === id)
@@ -65,7 +76,7 @@ app.delete("/repositories/:id", (request, response) => {
      return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", validateId ,(request, response) => {
   const { id } = request.params;
 
     const repository = repositories.find( repository => repository.id === id);
